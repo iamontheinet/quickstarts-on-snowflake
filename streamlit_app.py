416443c48c469d4134cdc81577a7cee0388e98e8
Dash Desai <iamontheinet@gmail.com>
2025-04-21 11:51:36 -0700

    Updated QS list and app to filter Archived guides

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-DEBUG = False
+DEBUG = True
 if DEBUG:
     DATA_FILE = 'qs_test.csv'
 else:
@@ -81,7 +81,17 @@ def display_cards(search_qs='',selected_status='All',selected_order_by='Last upd
         col_idx = 8
     
     with open(DATA_FILE) as csvfile:
-        data = list(csv.reader(csvfile))
+        org_list = list(csv.reader(csvfile))
+        data = []
+        for qs in org_list:
+            qs_title = qs[1]
+            qs_authors = qs[3]
+            qs_status = qs[6]
+            if (qs_status != 'Archived') and ((search_qs == '') or (search_qs in qs_title.lower() or search_qs in qs_authors.lower())) and (qs_status.lower() == selected_status.lower() or selected_status == 'All'):
+                data.append(qs)
+
+        st.caption(f"Number of matching QS guides: {len(data)}")
+
         reverse_order = selected_order == "Desc"
         if col_idx <= 3:
             sorted_data = sorted(data, key=lambda row: row[col_idx], reverse=reverse_order)
@@ -91,7 +101,7 @@ def display_cards(search_qs='',selected_status='All',selected_order_by='Last upd
             except:
                 sorted_data = sorted(data, key=lambda row: row[col_idx], reverse=reverse_order)
                 
-    if True:
+    if DEBUG:
         print(f"\nUser params >> Search: {search_qs} | Status: {selected_status} | Order by: {selected_order_by} | Order: {selected_order}\n")
         print(sorted_data[:10])
     
@@ -112,32 +122,31 @@ def display_cards(search_qs='',selected_status='All',selected_order_by='Last upd
         qs_md_last_updated = qs[8]
         qs_md_last_updated_by = qs[9]
 
-        if ((search_qs == '') or (search_qs in qs_title.lower() or search_qs in qs_authors.lower())) and (qs_status.lower() == selected_status.lower() or selected_status == 'All'):
-            with p_container:
-                col = col1 if col_index == 0 else col2 if col_index == 1 else col3 
+        with p_container:
+            col = col1 if col_index == 0 else col2 if col_index == 1 else col3 
 
-                col.write("<div style='border:1px solid #29b5e8'>", unsafe_allow_html = True)
-                qs_title_link = f"<a class='{qs_status.lower()}' href='{qs_link}' target='_blank'>{qs_title}</a>"
-                col.markdown(f" > {qs_title_link}", unsafe_allow_html = True)
-                col.markdown(f"<h6>Author(s): {qs_authors}</h6>", unsafe_allow_html = True)
-                col.markdown(f"<h6>Created on: {qs_md_first_updated}</h6>", unsafe_allow_html = True)
-                col.markdown(f"<h6>Last updated on: {qs_md_last_updated} | Last updated by: <a class='updated' href='https://github.com/{qs_md_last_updated_by}'>{qs_md_last_updated_by}</a> </h6>", unsafe_allow_html = True)
-                col.write("</div>", unsafe_allow_html = True)
+            col.write("<div style='border:1px solid #29b5e8'>", unsafe_allow_html = True)
+            qs_title_link = f"<a class='{qs_status.lower()}' href='{qs_link}' target='_blank'>{qs_title}</a>"
+            col.markdown(f" > {qs_title_link}", unsafe_allow_html = True)
+            col.markdown(f"<h6>Author(s): {qs_authors}</h6>", unsafe_allow_html = True)
+            col.markdown(f"<h6>Created on: {qs_md_first_updated}</h6>", unsafe_allow_html = True)
+            col.markdown(f"<h6>Last updated on: {qs_md_last_updated} | Last updated by: <a class='updated' href='https://github.com/{qs_md_last_updated_by}'>{qs_md_last_updated_by}</a> </h6>", unsafe_allow_html = True)
+            col.write("</div>", unsafe_allow_html = True)
 
-                with col.expander(label='Summary'):
-                    st.write(qs_summary)
-                    st.markdown("___")
-                    st.write(f"Id: {qs_id}")
-                    st.write(f"Categories: {qs_categories}")
-                    st.write(f"Status: {qs_status}")
-                
-            if (i % 3) == 0:
-                col1, col2, col3 = st.columns(3, gap='small')
-                p_container = st.container()
-                col_index = 0
-            else:
-                col_index += 1
-            i += 1
+            with col.expander(label='Summary'):
+                st.write(qs_summary)
+                st.markdown("___")
+                st.write(f"Id: {qs_id}")
+                st.write(f"Categories: {qs_categories}")
+                st.write(f"Status: {qs_status}")
+            
+        if (i % 3) == 0:
+            col1, col2, col3 = st.columns(3, gap='small')
+            p_container = st.container()
+            col_index = 0
+        else:
+            col_index += 1
+        i += 1
 
     if DEBUG:
         print("done displaying qs cards!")
